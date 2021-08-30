086424cb875cdf2441f293fb2e8b3f738a131e75
mtwickett <88279429+mtwickett@users.noreply.github.com>
2021-08-30 11:27:14 +0100

    Add files via upload

---
+++ 7 Kyu python solutions/Words to sentence.py
@@ -0,0 +1,8 @@
+# Write function which will create a string from a list of strings, separated by space.
+
+# Example:
+
+# ["hello", "world"] -> "hello world"
+
+def words_to_sentence(words):
+    return ' '.join(words)
