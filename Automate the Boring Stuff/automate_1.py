ca9b6b16d63fbdaea283d45fb5e1eb56c174ba45
mtwickett <88279429+mtwickett@users.noreply.github.com>
2022-08-07 14:31:37 +0100

    Create automate_1.py

---
+++ Automate the Boring Stuff/automate_1.py
@@ -0,0 +1,6 @@
+print('Hello World')
+my_name = input('What is your name?: ')
+print('It is good to meet you, ' + my_name)
+print(f'The length of your name is: {len(my_name)}')
+my_age = input('What is you age?: ')
+print('You will be ' + str(int(my_age) + 1) + ' in one year')
